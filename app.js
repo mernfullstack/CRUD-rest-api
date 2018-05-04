90bed553bb332a49c3c27c2c37e2846d864d3bdf
reactexcel <arun.etech2011@gmail.com>
2018-05-04 12:18:49 +0530

    changes for heroku

--- app.js
+++ app.js
@@ -18,15 +18,15 @@ app.use('/user',userRoutes);
 app.use((req,res,next)=>{
     const error = new Error('Not Found');
     error.status = 404;
-    next(error);
     console.log(error);
+    next(error);
 })
 
 app.use((error,req,res,next) => {
         res.status(error.status || 500);
         res.json({
             error: {
-                message : error.message
+                message : error
             }
         })
 })
