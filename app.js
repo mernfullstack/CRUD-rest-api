45c831dc1319a8d2aadc5d45608c856b2de7779e
mernfullstack <38958039+mernfullstack@users.noreply.github.com>
2018-05-04 12:00:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,6 @@ app.use((req,res,next)=>{
 app.use((error,req,res,next) => {
         res.status(error.status || 500);
         res.json({
-            console.log(error);
             error: {
                 message : error.message
             }
